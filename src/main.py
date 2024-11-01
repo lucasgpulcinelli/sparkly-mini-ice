b83ecf0b5b5de229c1ba3b4643ae21cdf23edd51
lucasgpulcinelli <lucasegp@usp.br>
2024-11-01 10:20:10 -0300

    add initial query on uk housing dataset, does not seem to use iceberg partitioning well

--- src/main.py
+++ src/main.py
@@ -8,7 +8,23 @@ spark = (
     .getOrCreate()
 )
 
-df = spark.read.format("iceberg").load("testtable")
+df = spark.sql("""
+    WITH max_no_transfers AS (
+        SELECT
+            COUNT(*) AS no_of_transfers, postcode
+        FROM testtable
+        WHERE City = 'LONDON' AND postcode IS NOT NULL
+        GROUP BY postcode
+        ORDER BY no_of_transfers DESC
+        LIMIT 1
+    )
+    SELECT t.Transaction_unique_identifier, t.postcode, t.Date_of_Transfer, t.price
+    FROM testtable t
+        JOIN max_no_transfers m ON t.postcode = m.postcode
+    ORDER BY t.Date_of_Transfer DESC
+""")
 
-df.show()
+arr = df.collect()
 
+for row in arr:
+    print(row)
